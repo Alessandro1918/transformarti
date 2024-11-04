f9e26017ba2138a5cbb95d7e79eb090ee9a327bc
Alessandro1918 <alessandro1918@hotmail.com>
2024-11-04 00:11:48 -0300

    style: added logo, font & new colors @ header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
+import { Raleway } from "next/font/google"
 import "./globals.css"
 
 const geistSans = localFont({
@@ -11,6 +12,11 @@ const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+})
+const raleway = Raleway({
+  weight: "500",
+  subsets: ["latin"],
+  variable: "--font-raleway"
 })
 
 export const metadata: Metadata = {
@@ -26,7 +32,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`
+          ${geistSans.variable} 
+          ${geistMono.variable} 
+          ${raleway.variable} 
+          antialiased
+        `}
       >
         {children}
       </body>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,21 +2,19 @@ import { FaBars } from "react-icons/fa"
 
 export function Header() {
   return (
-    <div className="flex w-full h-16 items-center justify-between px-4 bg-blue-400">
-
-      <div className="flex justify-center w-96">
-        <p className="text-5xl text-blue-900">
-          TransformArti
-        </p>
-      </div>
+    <div className="flex w-full h-32 items-center justify-between px-4 bg-blue-100">
+      <img 
+        src="/assets/logo.png"
+        className="w-96"
+      />
 
       <div className="flex flex-1 flex-row justify-center gap-4 max-sm:hidden">
-        <p className="text-xl text-white font-bold">Equipe</p>
-        <p className="text-xl text-white font-bold">Tratamentos</p>
+        <p className="text-2xl text-blue-900 font-raleway">Equipe</p>
+        <p className="text-2xl text-blue-900 font-raleway">Tratamentos</p>
       </div>
 
       <div className="sm:hidden">
-        <FaBars className="size-6 fill-white"/>
+        <FaBars className="size-6 fill-blue-900"/>
       </div>
     </div>
   )
