9295d11e1d7165ba06ff69e1a0ef39d266b5362c
Alessandro1918 <alessandro1918@hotmail.com>
2025-03-26 00:21:07 -0300

    doc: updated procedures images

--- src/requests/get-procedures.ts
+++ src/requests/get-procedures.ts
@@ -1,17 +1,17 @@
 export function getProcedures() {
   return [
     {
-      image: "/assets/procedures/clareamento.jpg",
+      image: "/assets/procedures/restauracao.jpg",
       title: "Restaurações",
       description: "As restaurações, também conhecidas como obturações, são procedimentos que reparam os dentes danificados por cáries ou fraturas. Também realizamos restaurações estéticas para corrigir pequenos dentes quebrados ou fechamento de diastemas. Utilizamos materiais de alta qualidade para devolver a função e a estética do seu sorriso."
     },
     {
-      image: "/assets/procedures/canal.jpg",
+      image: "/assets/procedures/ortodontia.jpg",
       title: "Ortodontia",
       description: "Através de aparelhos fixos, removíveis ou alinhadores Invisalign, corrigimos problemas como dentes tortos, desalinhados com espaços entre eles, problemas de mordida. Atendimento para crianças, adolescentes e adultos. A ortodontia pode melhorar sua saúde bucal e autoestima!"
     },
     {
-      image: "/assets/procedures/protese.jpg",
+      image: "/assets/procedures/cirurgia-ortognatica.jpg",
       title: "Cirurgia ortognática",
       description: "O tratamento visa a correção dos ossos da maxila e/ou mandíbula para pacientes que apresentam assimetria facial, prognatismo, retrognatismo, mordida aberta, dentre outros. O planejamento e o tratamento são alinhados em conjunto pelo ortodontista e pelo cirurgião bucomaxilofacial."
     },
@@ -26,12 +26,12 @@ export function getProcedures() {
       description: "Reabilitação da mastigação e do sorriso para dentes fraturados, desgastados, fragilizados por tratamento de canal/ cárie extensa, ou com estética insatisfatória.  Podem ser coroas, onlays, facetas e lentes de contato. Também realizamos próteses parciais removíveis (para perda de alguns dentes) e prótese total."
     },
     {
-      image: "/assets/procedures/extracao-siso.jpg",
+      image: "/assets/procedures/endodontia.jpg",
       title: "Endodontia",
       description: "O tratamento de canal é um procedimento indicado por um dentista para dentes com ou sem dor, que sofreram cárie extensa ou trauma. Consiste em remoção da polpa dentária danificada, limpeza e desinfecção do canal radicular (espaço onde ficava a polpa) e preenchimento do canal com um material obturador. Depois o dente precisa ser finalizado com uma restauração ou coroa, para recuperar sua função e estética."
     },
     {
-      image: "/assets/procedures/coroa.jpg",
+      image: "/assets/procedures/periodontia.jpg",
       title: "Periodontia",
       description: "A periodontia é a especialidade da odontologia que cuida da saúde das gengivas. Através de procedimentos de raspagem dos dentes o dentista atua na prevenção, diagnóstico e tratamento de doenças como gengivite e periodontite. Também realizamos cirurgia de enxerto de gengiva e gengivoplastia."
     },
@@ -41,14 +41,19 @@ export function getProcedures() {
       description: "O clareamento dental é um tratamento estético que tem como objetivo clarear os dentes, removendo manchas e amarelamento, proporcionando assim um sorriso mais branco, brilhante e jovem."
     },
     {
-      image: "/assets/procedures/coroa.jpg",
+      image: "/assets/procedures/harmonizacao-facial.jpg",
       title: "Harmonização facial",
       description: "Procedimentos minimamente invasivos que visam deixar o rosto mais bonito. Podem reduzir rugas e marcas de expressão; dar mais definição aos lábios, queixo e região malar; melhorar o aspecto da pele. São eles aplicação de toxina botulínica, preenchedores e bioestimuladores."
     },
     {
-      image: "/assets/procedures/invisalign.jpg",
+      image: "/assets/procedures/prevencao.jpg",
       title: "Prevenção",
       description: "Consultas de avaliação da saúde bucal onde são avaliados os dentes e demais estruturas da boca. O check up odontológico deve ser feito a cada 6 meses para identificar o quanto antes problemas como cáries, gengivite, tártaro e outras doenças."
+    },
+    {
+      image: "/assets/procedures/extracao.jpg",
+      title: "Extração",
+      description: "Procedimento feito por especialista em cirurgias, que consiste na remoção de dentes por falta de espaço, fratura, com cáries extensas ou até dentes do siso impactados. Visamos o conforto e bem estar do paciente em ambiente acolhedor."
     }
   ]
 }
